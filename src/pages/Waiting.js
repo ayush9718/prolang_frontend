de6f57ec98f6e22c44e4688a884a032de022159c
ayush971 <ak8287824629@gmail.com>
2024-11-06 14:47:14 +0530

    empty

--- src/pages/Waiting.js
+++ src/pages/Waiting.js
@@ -5,26 +5,35 @@
 
   export const Waiting = () => {
 
-    const [Room,setRoom]= useState({
-      _id:-1,
-      status:"waiting"
-    });
+    const [Room,setRoom]= useState(null);
 
 
-    function createRoom() {
-      return fetch(`http://localhost:4000/prolang/createRoom`, {
-        method: "POST",
-      }).then((response) => response.json());
+    async function createRoom() {
+      try{const response= await fetch(`http://localhost:4000/prolang/createRoom`, {method: "POST",});
+      const data  = await response.json();
+      return data}
+      catch(err){
+        console.log("error in api createroom");
+        console.error(err);
+      }
     }
     
-    function getRandomRoom() {
-      return fetch(`http://localhost:4000/prolang/randRoom`).then((response) =>
-        response.json()
-      );
+   async function getRandomRoom() {
+      try{
+        const response= await fetch(`http://localhost:4000/prolang/randRoom`);
+      const data = await response.json();
+      return data;
+      
+      }
+      catch(err){
+        console.log("error in api getrandomroom");
+        console.error(err);
+      
+      }
     }
     async function connectToARoom() {
-      const { randroom } = await getRandomRoom();
-
+      const randroom  = await getRandomRoom();
+        randroom= randroom.room;
       if (randroom._id !==-1) {
         setRoom(randroom);
       } else {
@@ -34,7 +43,7 @@
     }
     return (
       <div className='h-96 flex flex-col  justify-center items-center'>
-          <p>{Room._id}</p>
+          <p>{Room?._id}</p>
           <button onClick={connectToARoom}>start/connect</button>
       </div>
     )
